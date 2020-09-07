6cdceee2ef242bab6809bdadfcf268d9ca86ab9f
Peter Kha <khapeterk@gmail.com>
2020-09-07 11:53:16 -0400

    refactored

--- main.ts
+++ main.ts
@@ -1,16 +1,13 @@
 export default function sameNecklace(a: string, b: string): boolean {
-	if (a.length < 2 || b.length < 2) {
-		return a === b;
-	}
 	let isSameNecklace: boolean = false;
 	let aPushed: string = a;
-	for (let index: number = 1; index < a.length; index++) {
+	for (let index: number = 0; index < a.length; index++) {
 		aPushed = slideOneLetter(aPushed);
 		if (aPushed === b) {
 			isSameNecklace = true;
 		}
 	}
-	return isSameNecklace;
+	return a === b || isSameNecklace;
 }
 
 function slideOneLetter(a: string): string {
